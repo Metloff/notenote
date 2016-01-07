01243beeeb63e1ef8da10f73fb9e3eb7e3bf020c
Henry <genrih.metlov@gmail.com>
2016-01-07 12:17:37 +0000

    Generate notes controller and CRUD

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,34 @@
+class NotesController < ApplicationController
+  before_action :find_note, except: [:index, :new, :create]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+end
